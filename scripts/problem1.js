2d35f3b74d6ccaede4451b67d13fdc78bc3107d4
David Crichlow <dcrichlo.spsu@gmail.com>
2016-11-07 00:13:17 -0500

    Made alot of changes. Aadded css, images, and updated all files

--- scripts/problem1.js
+++ scripts/problem1.js
@@ -1,12 +1,22 @@
+/*document.getElementById("num1")
+	.addEventListener("keyup", funtion(event)
+	{
+		event.preventDefault();
+		if (event.keyCode == 13)
+		{
+			document.getElementById("enter1").click();
+		}
+	});*/
+
 var euler1 = function  ()
 {
 	var sumEu = 0;
 	var n = document.getElementById("num1").value;
 	var i = 0;
-	
+
 	for (i = 1; i < n; i++)
 	{
-		if (i % 3 === 0 || i % 5 == 0)
+		if (i % 3 == 0 || i % 5 == 0)
 		{
 			sumEu += i;
 		}
@@ -17,3 +27,4 @@ var euler1 = function  ()
 
 	answer1.innerHTML = sumEu;
 }
+
